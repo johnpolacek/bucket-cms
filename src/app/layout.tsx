2da675007a9c4ff0fb2552f2247bdabdc68aefd3
John Polacek <johnpolacek@gmail.com>
2023-08-29 06:21:30 -0500

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
