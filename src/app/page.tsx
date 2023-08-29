025c90cdb3201a42fea01fdb5896c4f4f9c15594
John Polacek <johnpolacek@gmail.com>
2023-08-29 05:45:10 -0500

    update main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-12 p-24">
       <p>Sometimes you don’t need a CMS. How about a Bucket?</p>
-      <p>Bucket is the world’s first portable drop-in CMS, no database required.</p>
-      <p>Own your data. All requests and all data stay on your existing infrastructure.</p>
-      <p>Automatically generated documentation of your content schema for developers to create custom components</p>
+      <p>Bucket is the world’s first portable drop-in CMS, no database necessary.</p>
+      <p>Own your data. All requests and all data stay with you.</p>
+      <p>Automatically generated documentation for developers to create custom components to display your data.</p>
       <div className="text-center">
         <p className="pb-4">Try the admin interface</p>
         <Link className="text-blue-700 underline" href="/demo">
