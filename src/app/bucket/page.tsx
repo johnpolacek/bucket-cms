9429e982b691dd7a5cde5b007e9985d97fdefe53
John Polacek <johnpolacek@gmail.com>
2023-09-04 18:01:39 -0500

    poc for reading component metadata from directory

--- src/app/bucket/page.tsx
+++ src/app/bucket/page.tsx
@@ -1,6 +1,34 @@
+import fs from "fs"
+import path from "path"
 import { Bucket } from "."
 
+type ComponentMetadata = {
+  componentName: string
+  supportedCollections: string[]
+  requiredFields: { name: string; typeName: string }[]
+}
+
 export default async function Home() {
+  console.log("fetchInstalledComponentsMetadata")
+
+  const fetchInstalledComponentsMetadata = (): ComponentMetadata[] => {
+    const componentsDirectory = path.join(process.cwd(), "src/app/bucket/src/components")
+    const componentDirs = fs.readdirSync(componentsDirectory)
+
+    const metadatas: ComponentMetadata[] = []
+    for (const dir of componentDirs) {
+      const metadataPath = path.join(componentsDirectory, dir, "metadata.json")
+      if (fs.existsSync(metadataPath)) {
+        const rawData = fs.readFileSync(metadataPath, "utf-8")
+        const metadata = JSON.parse(rawData)
+        metadatas.push(metadata)
+      }
+    }
+    console.log({ metadatas })
+    return metadatas
+  }
+  fetchInstalledComponentsMetadata()
+
   return (
     <main>
       <Bucket />
