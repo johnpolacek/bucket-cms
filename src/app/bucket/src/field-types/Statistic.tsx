8ae36dfa8c8e3dd77a445c71d052a8ff9b50f344
John Polacek <johnpolacek@gmail.com>
2023-09-08 20:25:19 -0500

    update render admin for statistic

--- src/app/bucket/src/field-types/Statistic.tsx
+++ src/app/bucket/src/field-types/Statistic.tsx
@@ -14,11 +14,15 @@ export type StatisticData = z.infer<typeof schema>
 export const Statistic: FieldType<StatisticData> = {
   renderAdmin: ({ data, setData }: FieldTypeProps<StatisticData>): ReactElement => {
     return (
-      <div>
-        <Label className="pl-px opacity-60 text-xs mt-2">Value</Label>
-        <Input type="text" defaultValue={data?.value || ""} onChange={(e) => setData && setData({ value: e.target.value, metric: data.metric })} />
-        <Label className="pl-px opacity-60 text-xs mt-2">Metric</Label>
-        <Input type="text" defaultValue={data?.metric || ""} onChange={(e) => setData && setData({ metric: e.target.value, value: data.value })} />
+      <div className="grid grid-cols-2 gap-2">
+        <div>
+          <Label className="pl-px opacity-60 text-xs mt-2">Value</Label>
+          <Input type="text" defaultValue={data?.value || ""} onChange={(e) => setData && setData({ value: e.target.value, metric: data.metric })} />
+        </div>
+        <div>
+          <Label className="pl-px opacity-60 text-xs mt-2">Metric</Label>
+          <Input type="text" defaultValue={data?.metric || ""} onChange={(e) => setData && setData({ metric: e.target.value, value: data.value })} />
+        </div>
       </div>
     )
   },
