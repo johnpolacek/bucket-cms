7ae413a31dd66ad97682523864fa5a9ce6a3c2df
John Polacek <johnpolacek@gmail.com>
2023-09-17 16:51:36 -0500

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,9 @@ export default function Home() {
         <GithubLink />
       </Header>
       <main className="flex min-h-screen flex-col items-center gap-8 py-16 px-8">
-        <h1 className="text-4xl font-light text-center text-blue-400">
-          Sometimes, you don’t need a <strong>CMS</strong>.
-        </h1>
+        <h1 className="text-3xl font-light text-center text-blue-500 pb-2">Maybe you don’t need a big cloud CMS</h1>
         <h2 className="text-6xl font-extrabold -mt-6 text-blue-700 pb-4">How about a Bucket?</h2>
-        <p className="text-lg opacity-70 max-w-[590px] text-center pb-8">Bucket is the world’s first portable drop-in CMS designed for Next.js (no database necessary)</p>
+        <p className="text-lg opacity-70 max-w-[700px] text-center px-4 pb-8">Bucket is the world’s first portable drop-in headless CMS designed for Next.js (no database necessary)</p>
         <div className="grid grid-cols-2 gap-4 -mt-4">
           <div className="col-span-2">
             <CLICommandCopy commandText="npx create-bucket-cms" />
@@ -27,28 +25,65 @@ export default function Home() {
             <Button className="w-full bg-blue-600 text-white text px-6 py-3 h-auto hover:bg-blue-700 hover:scale-105 transition-all ease-in-out">Try the Demo</Button>
           </Link>
         </div>
-
-        <div className="w-[640px] p-2 bg-black rounded-lg overflow-hidden mx-auto">
-          <video width="100%" height="100%" loop muted autoPlay playsInline>
-            <source src="https://s3.amazonaws.com/bucket.build-demo/create-bucket-cms-demo.mp4" type="video/mp4" />
-            <img
-              src="https://s3.amazonaws.com/bucket.build-demo/create-bucket-cms-cli.jpg"
-              alt="The npx create-bucket-cms terminal command ads bucket cms to your Next.js project"
-              width="1041 "
-              height="599"
-            />
-          </video>
+        <div className="grid grid-cols-2 pt-8 my-16 max-w-[960px] mx-auto px-4">
+          <div className="flex flex-col gap-8 justify-center pb-8 pr-12">
+            <h3 className="text-3xl font-bold text-blue-600">Break Free from the Cloud and Own Your Data</h3>
+            <p className="text-lg opacity-70">
+              Will Bucket CMS, the admin experience lives in your project, not in the cloud. No monthly fees. All your data, stored securely on your own S3 bucket as structured flat file JSON data.
+            </p>
+          </div>
+          <div className="w-[640px] p-4 bg-black rounded-lg overflow-hidden mx-auto opacity-80">
+            <video width="100%" height="100%" loop muted autoPlay playsInline>
+              <source src="https://s3.amazonaws.com/bucket.build-demo/create-bucket-cms-demo.mp4" type="video/mp4" />
+              <img
+                src="https://s3.amazonaws.com/bucket.build-demo/create-bucket-cms-cli.jpg"
+                alt="The npx create-bucket-cms terminal command ads bucket cms to your Next.js project"
+                width="1041 "
+                height="599"
+              />
+            </video>
+          </div>
         </div>
 
-        <p className="pt-32 mt-32">Own your data. All of your requests and data stay with you.</p>
-        <p>You own the CMS. The content, the files, the APIs are all yours and live on your system. You can even name the CMS whatever you want.</p>
-        <p>Automatically generated documentation for developers to create custom components to display your data.</p>
-        <p>Bucket CMS brings you the flexibility of a flat file JSON database meets the TypeScript-first schema validation of Zod.</p>
-        <div className="text-center">
-          <p className="pb-4">Try the admin interface</p>
-          <Link className="text-blue-700 underline" href="/demo">
-            Go to Demo
-          </Link>
+        <div className="grid grid-cols-2 pt-8 my-16 max-w-[960px] mx-auto px-4">
+          <div className="flex flex-col gap-8 justify-center pb-8 pr-12">
+            <h3 className="text-3xl font-bold text-blue-600">The Drop-In Headless CMS: No Database, No Problem.</h3>
+            <p className="text-lg opacity-70">
+              Introducing the drop-in headless CMS experience for Next.js. On-the-fly data schema. Type-safe validation with Zod. Automatically generated documentation. Streamlined development.
+            </p>
+          </div>
+          <div className="w-[640px] p-4 bg-black rounded-lg overflow-hidden mx-auto opacity-80">
+            <video width="100%" height="100%" loop muted autoPlay playsInline>
+              <source src="https://s3.amazonaws.com/bucket.build-demo/create-bucket-cms-demo.mp4" type="video/mp4" />
+              <img
+                src="https://s3.amazonaws.com/bucket.build-demo/create-bucket-cms-cli.jpg"
+                alt="The npx create-bucket-cms terminal command ads bucket cms to your Next.js project"
+                width="1041 "
+                height="599"
+              />
+            </video>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 pt-8 my-16 max-w-[960px] mx-auto px-4">
+          <div className="flex flex-col gap-8 justify-center pb-8 pr-12">
+            <h3 className="text-3xl font-bold text-blue-600">
+              Integrate Bucket CMS <br />
+              into your Next.js project with a single command
+            </h3>
+            <p className="text-lg opacity-70">Our CLI tool automates the setup process, providing you with intelligent default settings tailored to your project.</p>
+          </div>
+          <div className="w-[640px] p-4 bg-black rounded-lg overflow-hidden mx-auto opacity-80">
+            <video width="100%" height="100%" loop muted autoPlay playsInline>
+              <source src="https://s3.amazonaws.com/bucket.build-demo/create-bucket-cms-demo.mp4" type="video/mp4" />
+              <img
+                src="https://s3.amazonaws.com/bucket.build-demo/create-bucket-cms-cli.jpg"
+                alt="The npx create-bucket-cms terminal command ads bucket cms to your Next.js project"
+                width="1041 "
+                height="599"
+              />
+            </video>
+          </div>
         </div>
       </main>
     </>
