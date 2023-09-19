725c3c011e2787365fd8dd75454347d74d48cb8c
John Polacek <johnpolacek@gmail.com>
2023-09-19 05:54:21 -0500

    fix syntax

--- src/app/bucket/src/ui/icon/edit.tsx
+++ src/app/bucket/src/ui/icon/edit.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 const EditIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
       <path
         stroke-linecap="round"
         stroke-linejoin="round"
