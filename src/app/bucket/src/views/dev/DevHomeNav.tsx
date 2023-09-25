c9b257ffec0ab47d94570d5b339abcd4a3c33538
John Polacek <johnpolacek@gmail.com>
2023-09-25 17:07:58 -0500

    add hover state to dev nav

--- src/app/bucket/src/views/dev/DevHomeNav.tsx
+++ src/app/bucket/src/views/dev/DevHomeNav.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { CollectionFieldsData } from "../../types"
+import { cn } from "../../ui/utils"
 
 function DevHomeNav({ collections, activeSection }: { collections: CollectionFieldsData[]; activeSection: string }) {
   return (
@@ -8,34 +9,70 @@ function DevHomeNav({ collections, activeSection }: { collections: CollectionFie
       <ul className="flex flex-col gap-8">
         <li>
           <h3 className="font-semibold pb-4 text-xs uppercase opacity-50">Overview</h3>
-          <ul className="flex flex-col gap-3 text-sm pl-2">
+          <ul className="flex flex-col gap-3 text-sm">
             <li>
-              <a className={activeSection === "introduction" ? "text-blue-600 font-semibold" : "text-black opacity-70"} href="#introduction">
+              <a
+                className={cn(
+                  "px-2 py-2 bg-gray-50 hover:bg-gray-200 rounded transition-all duration-500 ease-in-out",
+                  activeSection === "introduction" ? "text-blue-600 font-semibold" : "text-black opacity-70"
+                )}
+                href="#introduction"
+              >
                 Introduction
               </a>
             </li>
             <li>
-              <a className={activeSection === "admin" ? "text-blue-600 font-semibold" : "text-black opacity-70"} href="#admin">
+              <a
+                className={cn(
+                  "px-2 py-2 bg-gray-50 hover:bg-gray-200 rounded transition-all duration-500 ease-in-out",
+                  activeSection === "admin" ? "text-blue-600 font-semibold" : "text-black opacity-70"
+                )}
+                href="#admin"
+              >
                 Admin
               </a>
             </li>
             <li>
-              <a className={activeSection === "collections" ? "text-blue-600 font-semibold" : "text-black opacity-70"} href="#collections">
+              <a
+                className={cn(
+                  "px-2 py-2 bg-gray-50 hover:bg-gray-200 rounded transition-all duration-500 ease-in-out",
+                  activeSection === "collections" ? "text-blue-600 font-semibold" : "text-black opacity-70"
+                )}
+                href="#collections"
+              >
                 Collections
               </a>
             </li>
             <li>
-              <a className={activeSection === "fields" ? "text-blue-600 font-semibold" : "text-black opacity-70"} href="#fields">
+              <a
+                className={cn(
+                  "px-2 py-2 bg-gray-50 hover:bg-gray-200 rounded transition-all duration-500 ease-in-out",
+                  activeSection === "fields" ? "text-blue-600 font-semibold" : "text-black opacity-70"
+                )}
+                href="#fields"
+              >
                 Fields
               </a>
             </li>
             <li>
-              <a className={activeSection === "field-types" ? "text-blue-600 font-semibold" : "text-black opacity-70"} href="#field-types">
+              <a
+                className={cn(
+                  "px-2 py-2 bg-gray-50 hover:bg-gray-200 rounded transition-all duration-500 ease-in-out",
+                  activeSection === "field-types" ? "text-blue-600 font-semibold" : "text-black opacity-70"
+                )}
+                href="#field-types"
+              >
                 Field Types
               </a>
             </li>
             <li>
-              <a className={activeSection === "items" ? "text-blue-600 font-semibold" : "text-black opacity-70"} href="#items">
+              <a
+                className={cn(
+                  "px-2 py-2 bg-gray-50 hover:bg-gray-200 rounded transition-all duration-500 ease-in-out",
+                  activeSection === "items" ? "text-blue-600 font-semibold" : "text-black opacity-70"
+                )}
+                href="#items"
+              >
                 Items
               </a>
             </li>
@@ -43,15 +80,27 @@ function DevHomeNav({ collections, activeSection }: { collections: CollectionFie
         </li>
         <li>
           <h3 className="font-semibold pb-4 text-xs uppercase opacity-50">Your Collections</h3>
-          <ul className="flex flex-col gap-3 text-sm pl-2">
+          <ul className="flex flex-col gap-3 text-sm">
             <li>
-              <a className={activeSection === "fetching-data" ? "text-blue-600 font-semibold" : "text-black opacity-70"} href="#fetching-data">
+              <a
+                className={cn(
+                  "px-2 py-2 bg-gray-50 hover:bg-gray-200 rounded transition-all duration-500 ease-in-out",
+                  activeSection === "fetching-data" ? "text-blue-600 font-semibold" : "text-black opacity-70"
+                )}
+                href="#fetching-data"
+              >
                 Fetching Data
               </a>
             </li>
             {collections.map((collection) => (
               <li key={collection.name}>
-                <a className={activeSection === collection.name ? "text-blue-600 font-semibold" : "text-black opacity-70"} href={"#" + collection.name}>
+                <a
+                  className={cn(
+                    "px-2 py-2 bg-gray-50 hover:bg-gray-200 rounded transition-all duration-500 ease-in-out",
+                    activeSection === collection.name ? "text-blue-600 font-semibold" : "text-black opacity-70"
+                  )}
+                  href={"#" + collection.name}
+                >
                   {collection.name}
                 </a>
               </li>
