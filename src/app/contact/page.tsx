6917d2566e9c15ed61b9eecbef22ee555dd41efd
John Polacek <johnpolacek@gmail.com>
2023-10-04 20:06:09 -0500

    Remove unnecessary header and GitHub link from contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,4 @@
-import Header from "../demo/components/ui/header"
 import Footer from "../demo/components/ui/footer"
-import GithubLink from "../demo/components/ui/github-link"
 import { BrandImage, PageHeading } from "../bucket/src/views/brand"
 import ContactForm from "../demo/components/views/ContactForm"
 import TransitionWrapper from "../bucket/src/views/admin/TransitionWrapper"
@@ -8,9 +6,6 @@ import TransitionWrapper from "../bucket/src/views/admin/TransitionWrapper"
 export default function Contact() {
   return (
     <main>
-      <Header>
-        <GithubLink />
-      </Header>
       <div className="py-12 px-8 border-t max-w-[480px] mx-auto text-center">
         <TransitionWrapper>
           <BrandImage />
