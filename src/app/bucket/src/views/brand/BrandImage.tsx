d931390bccf42a6dfe40ba8738b1c32342c4a37b
John Polacek <johnpolacek@gmail.com>
2023-10-13 15:55:56 -0500

    Update AdminFooter and BrandImage components, and add BrandImage to page.tsx

--- src/app/bucket/src/views/brand/BrandImage.tsx
+++ src/app/bucket/src/views/brand/BrandImage.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
+import { cn } from "../../ui/utils"
 
-const BrandImage = () => {
-  return <Image className="opacity-90 rounded-full overflow-hidden border border-blue-300 mx-auto mb-2" src="/bucket-cms-logo.png" width={210} height={210} alt="" />
+const BrandImage = ({ className }: { className: string }) => {
+  return <Image className={cn("opacity-90 rounded-full overflow-hidden border border-blue-300 mx-auto mb-2", className)} src="/bucket-cms-logo.png" width={210} height={210} alt="" />
 }
 
 export { BrandImage }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@ import Link from "next/link"
 import { Button } from "./demo/components/ui/button"
 import CLICommandCopy from "./demo/components/ui/cli-command-copy"
 import ExamplesView from "./demo/examples/examples-view"
+import { BrandImage } from "./bucket/src/views/brand"
 
 export default function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center gap-6 sm:gap-8 py-8 sm:py-20 px-2 sm:px-8 mb-8">
-        <h1 className="text-base sm:text-xl md:text-3xl font-light text-center text-blue-500 pb-2">Maybe you don’t need a big cloud CMS...</h1>
+        <div className="sm:h-72 w-full overflow-hidden -mt-12 mb-2 relative">
+          <div className="w-full h-1/4 absolute bottom-0 left-0 bg-gradient-to-t from-[rgba(255,255,255,1)] to-[rgba(255,255,255,0)] z-10"></div>
+          <div className="scale-80 sm:scale-150 relative top-8 sm:top-16">
+            <BrandImage className="border-[3px]" />
+          </div>
+        </div>
+        <h1 className="text-lg sm:text-xl md:text-3xl font-light text-center text-blue-500 pb-2">Maybe you don’t need a big cloud CMS...</h1>
         <h2 className="text-4xl sm:text-6xl text-center font-extrabold -mt-6 text-blue-700">How about a Bucket?</h2>
         <p className="sm:text-xl opacity-70 max-w-[960px] text-center px-4 pb-8 leading-normal sm:-mt-4">
           Bucket is the world’s first AI-Powered Portable Drop-in Headless CMS designed for Next.js <span className="italic block">(no database necessary)</span>
