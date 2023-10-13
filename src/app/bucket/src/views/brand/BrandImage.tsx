ba1d832d125609223b8a13d1f7a809f49d0637c4
John Polacek <johnpolacek@gmail.com>
2023-10-13 17:55:09 -0500

    Update BrandImage component to make the "className" prop optional

--- src/app/bucket/src/views/brand/BrandImage.tsx
+++ src/app/bucket/src/views/brand/BrandImage.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import { cn } from "../../ui/utils"
 
-const BrandImage = ({ className }: { className: string }) => {
+const BrandImage = ({ className }: { className?: string }) => {
   return <Image className={cn("opacity-90 rounded-full overflow-hidden border border-blue-300 mx-auto mb-2", className)} src="/bucket-cms-logo.png" width={210} height={210} alt="" />
 }
 
