bc4e7d33b1365e6aabc11bc6de4a459bf2bc2aab
John Polacek <johnpolacek@gmail.com>
2023-10-13 19:49:25 -0500

    Add default and NextAuth layouts for Bucket

---
+++ src/app/bucket/layout-nextauth.tsx
@@ -0,0 +1,23 @@
+import { getServerSession } from "next-auth/next"
+import { options } from "./options"
+import { BrandImage } from "./src/views/brand/BrandImage"
+import { AdminFooter } from "./src/views/admin/AdminFooter"
+import BucketProvider from "./src/views/providers/BucketProvider"
+
+export default async function BucketLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(options)
+
+  return (
+    <div className="flex flex-col grow justify-start items-center relative w-full h-full bg-white">
+      {session?.user?.name || process.env.NODE_ENV === "development" ? (
+        <BucketProvider>{children}</BucketProvider>
+      ) : (
+        <div className="w-screen h-screen flex flex-col justify-center items-center p-8 gap-2">
+          <BrandImage />
+          <p className="pb-8 italic opacity-70">Authentication required for access</p>
+        </div>
+      )}
+      <AdminFooter showAuthenticationWarning={!session?.user?.name && process.env.NODE_ENV === "development"} />
+    </div>
+  )
+}
