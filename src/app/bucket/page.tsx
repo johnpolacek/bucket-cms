ee442af5d36b5558fc8328805296e76e9ec3f7ab
John Polacek <johnpolacek@gmail.com>
2023-10-13 19:46:43 -0500

    update admin redirect

--- src/app/bucket/page.tsx
+++ src/app/bucket/page.tsx
@@ -1,5 +1,5 @@
-import { permanentRedirect } from "next/navigation"
+import { redirect } from "next/navigation"
 
 export default async function Home() {
-  return permanentRedirect("/bucket/admin")
+  return redirect("/bucket/admin")
 }
